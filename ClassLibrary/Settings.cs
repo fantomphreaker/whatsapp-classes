fcf7e21e768cbbf44a155276e1ca813710cea1ac
fantomphreaker <navaneethgopal4991@gmail.com>
2021-01-03 14:12:50 +0530

    added settings class

---
+++ ClassLibrary/Settings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Settings
+    {
+        public void changeNumber()
+        {
+
+        }
+
+        public void deleteAccount()
+        {
+
+        }
+
+        public void modifyPrivacySetting()
+        {
+
+        }
+
+        public void editProfilePicture()
+        {
+
+        }
+
+        public void editDisplayStatus()
+        {
+
+        }
+    }
+}
--- ClassLibrary/User.cs
+++ ClassLibrary/User.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace ClassLibrary
 {
-    class Class1
+    class User
     {
     }
 }
